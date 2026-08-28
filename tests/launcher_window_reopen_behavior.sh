8d0181fb395b632aae873bb443ae3183021e79fd
Wojtek <walkowo@gmail.com>
2026-08-28 17:46:19 +0200

    test(launcher): isolate updater preflight fixtures

--- tests/launcher_window_reopen_behavior.sh
+++ tests/launcher_window_reopen_behavior.sh
@@ -374,6 +374,7 @@ COMMON_ENV=(
     "HOME=$HOME_DIR"
     "XDG_RUNTIME_DIR=$RUNTIME_DIR"
     "CODEX_CLI_PATH=$(command -v true)"
+    "CODEX_UPDATE_MANAGER_PATH=$(command -v true)"
     "CODEX_WEBVIEW_PORT=$PORT"
 )
 
